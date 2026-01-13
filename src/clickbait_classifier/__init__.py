8a46283eb62eb63fcfe2ef167c22549b9e6b23f4
Kaja Hovinbøle <kajahovin@icloud.com>
2026-01-13 14:33:25 +0100

    readme and pre-commit

--- src/clickbait_classifier/__init__.py
+++ src/clickbait_classifier/__init__.py
@@ -6,7 +6,8 @@ from loguru import logger
 logger.remove()  # Remove default handler
 logger.add(
     lambda msg: print(msg, end=""),
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> "
+    "| <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     level="INFO",
     colorize=True,
 )
